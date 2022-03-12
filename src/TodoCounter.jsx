d4d4cde099caa9fbbd84586e02985288c50992d6
rosarie <rosariosanchezsampietro@gmail.com>
2022-03-11 21:47:51 -0300

    First commit: First version of CSS completed

---
+++ src/TodoCounter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './TodoCounter.css'
+
+
+// const styles = {
+//     color: 'red',
+//     backgroundColor: 'grey'
+// }
+const TodoCounter = () => {
+    return (
+       
+        <h2 className="TodoCounter">Completaste 2 todos, esto está hardcodeado</h2>
+    )
+}
+
+export default TodoCounter;
